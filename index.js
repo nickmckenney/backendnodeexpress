32c487bb27dce4c502c3a077d590497cbf5ad18c
nickmckenney <nick.mckenney@fool.com>
2020-09-25 19:12:36 -0400

    remove api

--- index.js
+++ index.js
@@ -13,50 +13,50 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
-app.get("/",(req,res)=>{
-    res.send("hi");
-});
-app.get('/api', (req, res) => {
-    res.send('API is Working!')
-});
+// app.get("/",(req,res)=>{
+//     res.send("hi");
+// });
+// app.get('/api', (req, res) => {
+//     res.send('API is Working!')
+// });
 
 
-app.post('/api/email', (req, res, next) => {
+// app.post('/api/email', (req, res) => {
 
-    console.log(req.body);
+//     console.log(req.body);
 
-    sendGrid.setApiKey('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');
-    const msg = {
-        to: 'mckenney2001@gmail.com',
-        from: 'mckenney2001@gmail.com',//SendGrid only allows to send emails to yourself
-        subject: req.body.email,
-        text: req.body.message
-    }
+//     sendGrid.setApiKey('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');
+//     const msg = {
+//         to: 'mckenney2001@gmail.com',
+//         from: 'mckenney2001@gmail.com',//SendGrid only allows to send emails to yourself
+//         subject: req.body.email,
+//         text: req.body.message
+//     }
 
-    sendGrid.send(msg)
-        .then(result => {
+//     sendGrid.send(msg)
+//         .then(result => {
 
-            res.status(200).json({
-                success: true
-            });
+//             res.status(200).json({
+//                 success: true
+//             });
 
-        })
-        .catch(err => {
+//         })
+//         .catch(err => {
 
-            console.log('error: ', err);
-            res.status(401).json({
-                success: false
-            });
+//             console.log('error: ', err);
+//             res.status(401).json({
+//                 success: false
+//             });
 
-        });
-});
+//         });
+// });
 
 
 
